30d0efefeb022cdc7256c20420cf52716292c7d9
Spenser Black <spenserblack01@gmail.com>
2025-11-11 21:48:52 -0500

    Document functions

--- src/words.h
+++ src/words.h
@@ -1,6 +1,15 @@
 #ifndef TODAYS_FORTUNE_WORDS_H
 #define TODAYS_FORTUNE_WORDS_H
+/**
+ * Returns a random noun.
+ */
 const char* get_random_noun();
+/**
+ * Returns a random verb.
+ */
 const char* get_random_verb();
+/**
+ * Returns a random adjective.
+ */
 const char* get_random_adjective();
 #endif
